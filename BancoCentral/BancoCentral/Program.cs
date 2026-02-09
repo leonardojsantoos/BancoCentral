723ea88cdfeed1b9938ddb9a4eebfcc5598a9bca
Leonardo de Jesus Santos <leonardojsantoos@gmail.com>
2026-02-09 10:52:05 -0300

    Classe adicionada ContaBancaria

--- BancoCentral/BancoCentral/Program.cs
+++ BancoCentral/BancoCentral/Program.cs
@@ -14,14 +14,23 @@ namespace MyApp
             minhaTransacao.Valor = 250.00m;
             minhaTransacao.Data = DateTime.Now;
             minhaTransacao.Tipo = "PIX";
+            minhaTransacao.ContaOrigem.Agente = "001";
+            minhaTransacao.ContaOrigem.Numero = "888";
 
+            minhaTransacao.ContaDestino.Agente = "002";
+            minhaTransacao.ContaDestino.Numero = "999";
+                               
             Console.WriteLine("--- REGISTRO BACEN ---\n\n" +
                 $"Banco: {meuBanco.Nome}\n" +
                 $"Codigo: {meuBanco.CodigoBacen}\n" +
-                $"---" +
+                $"---\n" +
                 $"Tipo: {minhaTransacao.Tipo}\n" +
                 $"Valor: R${minhaTransacao.Valor}\n" +
-                $"Data: {minhaTransacao.Data}\n");
+                $"Data: {minhaTransacao.Data}\n" +
+                $"---\n" +
+                $"Conta Origem: {minhaTransacao.ContaOrigem.Agente}\n" +
+                $"Conta Destino: {minhaTransacao.ContaDestino.Agente}");
+
         }
     }
 }
--- BancoCentral/BancoCentral/Transacao.cs
+++ BancoCentral/BancoCentral/Transacao.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BancoCentral;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BancoCentral
 {
@@ -11,5 +13,13 @@ namespace BancoCentral
         public decimal Valor { get; set; }
         public DateTime  Data { get; set; }
         public string Tipo { get; set; }
+        public ContaBancaria ContaOrigem { get; set; }
+        public ContaBancaria ContaDestino { get; set; }
+
+        public Transacao()
+        {
+            ContaOrigem = new ContaBancaria();
+            ContaDestino = new ContaBancaria();
+        }
     }
 }
