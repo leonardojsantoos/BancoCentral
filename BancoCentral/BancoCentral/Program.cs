ef5dfcda45aee0644d645dda0f4eadbcf8b75832
Leonardo de Jesus Santos <leonardojsantoos@gmail.com>
2026-02-09 09:36:55 -0300

    Git Ignore correto

---
+++ BancoCentral/BancoCentral/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BancoCentral;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco meuBanco = new Banco();
+            meuBanco.Nome = "Banco do Brasil";
+            meuBanco.CodigoBacen = "001";
+            Transacao minhaTransacao = new Transacao();
+            minhaTransacao.Valor = 250.00m;
+            minhaTransacao.Data = DateTime.Now;
+            minhaTransacao.Tipo = "PIX";
+
+            Console.WriteLine("--- REGISTRO BACEN ---\n\n" +
+                $"Banco: {meuBanco.Nome}\n" +
+                $"Codigo: {meuBanco.CodigoBacen}\n" +
+                $"---" +
+                $"Tipo: {minhaTransacao.Tipo}\n" +
+                $"Valor: R${minhaTransacao.Valor}\n" +
+                $"Data: {minhaTransacao.Data}\n");
+        }
+    }
+}
---
+++ BancoCentral/BancoCentral/Transacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCentral
+{
+    internal class Transacao
+    {
+        public decimal Valor { get; set; }
+        public DateTime  Data { get; set; }
+        public string Tipo { get; set; }
+    }
+}
